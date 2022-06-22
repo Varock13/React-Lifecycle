f589b8af45171966af4a1658fe17097b684f8e46
Alvaro Araya <aaraya_13@hotmail.com>
2022-06-21 19:16:18 -0600

    Se agerga proyecto Ciclo de Vida

---
+++ src/components/Controls.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Clock from './Clock';
+
+class Controls extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            active : false
+        };
+    }
+
+    render(){
+        return(
+            <>
+            <button onClick={  ()=> this.setState( { active: !this.state.active } ) } >
+                {this.state.value ? "On" : "Off"} 
+            </button>
+            {
+                this.state.active ? 
+                <Clock /> : 
+                <p>Clock is Off</p>
+            }
+            </>
+        );
+    }
+}
+
+export default Controls;
